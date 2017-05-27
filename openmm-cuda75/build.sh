35938be65d4f591d6327ab9e2b18ab143b0afc9f
John Chodera <john.chodera@choderalab.org>
2017-05-27 18:38:30 -0400

    clang 4.0 works!

--- openmm-cuda75/build.sh
+++ openmm-cuda75/build.sh
@@ -37,8 +37,9 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
     # CUDA OpenCL
     #CMAKE_FLAGS+=" -DOPENCL_INCLUDE_DUR=${CUDA_PATH}/include/"
     #CMAKE_FLAGS+=" -DOPENCL_LIBRARY=${CUDA_PATH}/lib64/libOpenCL.so"
-    CMAKE_FLAGS+=" -DCMAKE_C_FLAGS=--gcc-toolchain=/opt/rh/devtoolset-2/root/usr/"
+    #CMAKE_FLAGS+=" -DCMAKE_C_FLAGS=--gcc-toolchain=/opt/rh/devtoolset-2/root/usr/"
     CMAKE_FLAGS+=" -DCMAKE_CXX_FLAGS=--gcc-toolchain=/opt/rh/devtoolset-2/root/usr/"
+    #CMAKE_FLAGS+=" -D_CMAKE_TOOLCHAIN_PREFIX=x86_64-redhat-linux"
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     # conda-build MACOSX_DEPLOYMENT_TARGET must be exported as an environment variable to override 10.7 default
     # cc: https://github.com/conda/conda-build/pull/1561

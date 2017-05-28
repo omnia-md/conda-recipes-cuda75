4456e9154453d5f45a118e975519a86095e68e10
John Chodera <john.chodera@choderalab.org>
2017-05-28 13:43:12 -0400

    Try forcing upload to rc channel

--- devtools/docker-build.sh
+++ devtools/docker-build.sh
@@ -13,7 +13,7 @@ conda config --add channels conda-forge
 conda install -yq conda\>=4.3 conda-build=2.1.5 jinja2 anaconda-client
 
 #/io/conda-build-all -vvv --python $PY_BUILD_VERSION $UPLOAD -- /io/cuda75
-conda install -yq cuda75
-/io/conda-build-all -vvv --check-against "omnia/label/cuda75" --python $PY_BUILD_VERSION $UPLOAD -- /io/openmm-cuda75
+#conda install -yq cuda75
+/io/conda-build-all -vvv --check-against "omnia/label/cuda75" --python $PY_BUILD_VERSION $UPLOAD -- /io/cuda75 /io/openmm-cuda75
 
 #mv /anaconda/conda-bld/linux-64/*tar.bz2 /io/ || true

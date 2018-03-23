d4693716de4d830c15d41e55061352dee82e363d
John Chodera <john.chodera@choderalab.org>
2018-03-22 23:13:25 -0400

    Update build scripts

--- devtools/docker-build.sh
+++ devtools/docker-build.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
 set -e
 set -x
-
-# Activate devtools
-source /opt/docker/bin/entrypoint
-
 conda config --add channels omnia
-conda config --add channels omnia/label/cuda75
 # Move the conda-forge channel to the top
 # Cannot just append omnia otherwise default would have higher priority
 conda config --add channels conda-forge
-conda install -yq conda\>=4.3 conda-build=2.1.5 jinja2 anaconda-client
+conda update -yq conda
+conda install -yq conda-build==2.1.17 jinja2 anaconda-client
 
-#/io/conda-build-all -vvv --python $PY_BUILD_VERSION $UPLOAD -- /io/cuda75
-#conda install -yq cuda75
-/io/conda-build-all -vvv --check-against "omnia/label/cuda75" --python $PY_BUILD_VERSION $UPLOAD -- /io/cuda75 /io/openmm-cuda75
-
-#mv /anaconda/conda-bld/linux-64/*tar.bz2 /io/ || true
+/io/conda-build-all -vvv $UPLOAD -- /io/*
+#/io/conda-build-all -vvv --check-against "omnia/label/cuda75" --python $PY_BUILD_VERSION $UPLOAD -- /io/cuda75 /io/openmm-cuda75
--- devtools/osx-build.sh
+++ devtools/osx-build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e -x
 export MACOSX_DEPLOYMENT_TARGET="10.9"
-# Update homebrew
-brew uninstall -y brew-cask || brew untap -y caskroom/cask || 1
-brew update -y --quiet
-brew tap -y caskroom/cask
+# Clear existing locks
+rm -rf /usr/local/var/homebrew/locks
+# Update homebrew cant disable this yet, -y and --quiet do nothing
+brew update
 
 # Install Miniconda
 curl -s -O https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh;
@@ -12,7 +12,8 @@ bash Miniconda3-latest-MacOSX-x86_64.sh -b -p $HOME/anaconda;
 export PATH=$HOME/anaconda/bin:$PATH;
 conda config --add channels omnia;
 conda config --add channels conda-forge;
-conda install -yq conda\>=4.3 conda-env conda-build jinja2 anaconda-client;
+conda update -yq conda;
+conda install -yq conda-env conda-build==2.1.7 jinja2 anaconda-client;
 conda config --show;
 
 # Do this step last to make sure conda-build, conda-env, and conda updates come from the same channel first
@@ -24,18 +25,36 @@ if [ "$INSTALL_OPENMM_PREREQUISITES" = true ] ; then
     # Install OpenMM dependencies that can't be installed through
     # conda package manager (doxygen + CUDA)
     brew install -y https://raw.githubusercontent.com/Homebrew/homebrew-core/5b680fb58fedfb00cd07a7f69f5a621bb9240f3b/Formula/doxygen.rb
-    curl -O -s http://developer.download.nvidia.com/compute/cuda/${CUDA_VERSION}/Prod/network_installers/mac/x86_64/cuda_mac_installer_tk.tar.gz
-    curl -O -s http://developer.download.nvidia.com/compute/cuda/${CUDA_VERSION}/Prod/network_installers/mac/x86_64/cuda_mac_installer_drv.tar.gz
+    # Make the nvidia-cache if not there
+    mkdir -p $NVIDIA_CACHE
+    cd $NVIDIA_CACHE
+    # Download missing nvidia installers
+    if ! [ -f cuda_mac_installer_tk.tar.gz ]; then
+        curl -O -# http://developer.download.nvidia.com/compute/cuda/${CUDA_VERSION}/Prod/network_installers/mac/x86_64/cuda_mac_installer_tk.tar.gz
+    fi
+    if ! [ -f cuda_mac_installer_drv.tar.gz ]; then
+        curl -O -# http://developer.download.nvidia.com/compute/cuda/${CUDA_VERSION}/Prod/network_installers/mac/x86_64/cuda_mac_installer_drv.tar.gz
+    fi
     sudo tar -zxf cuda_mac_installer_tk.tar.gz -C /;
     sudo tar -zxf cuda_mac_installer_drv.tar.gz -C /;
-    rm -f cuda_mac_installer_tk.tar.gz cuda_mac_installer_drv.tar.gz
+    # Don't delete the tarballs to cache the package
+    # rm -f cuda_mac_installer_tk.tar.gz cuda_mac_installer_drv.tar.gz
+    # Now head back to work directory
+    cd $TRAVIS_BUILD_DIR
 
     # Install latex.
-    brew cask install -y basictex
     export PATH="/usr/texbin:${PATH}:/usr/bin"
-    sudo tlmgr update --self
+    brew cask install basictex
+    mkdir -p /usr/texbin
+    # Path based on https://github.com/caskroom/homebrew-cask/blob/master/Casks/basictex.rb location
+    # .../texlive/{YEAR}basic/bin/{ARCH}/{Location of actual binaries}
+    # Sym link them to the /usr/texbin folder in the path
+    export TLREPO=http://ctan.math.utah.edu/ctan/tex-archive/systems/texlive/tlnet
+    ln -s /usr/local/texlive/*basic/bin/*/* /usr/texbin/
+    sudo tlmgr --repository=$TLREPO update --self
     sleep 5
-    sudo tlmgr --persistent-downloads install titlesec framed threeparttable wrapfig multirow collection-fontsrecommended hyphenat xstring \
+    sudo tlmgr --persistent-downloads --repository=$TLREPO install \
+        titlesec framed threeparttable wrapfig multirow collection-fontsrecommended hyphenat xstring \
         fncychap tabulary capt-of eqparbox environ trimspaces
 fi;
 
